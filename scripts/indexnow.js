ab928c00aab2345a38a273382a86bb85c329e24a
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2024-11-16 19:25:18 +0100

    Index Now Fix
    A la tercera va la vencida?

---
+++ scripts/indexnow.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+dotenv.config();
+const myEnvVar = process.env.INDEXNOW_KEY;
+
+if (myEnvVar) {
+  const filePath = join('./public', `${myEnvVar}.txt`);
+  const fileContent = `${myEnvVar}`;
+  writeFileSync(filePath, fileContent, 'utf-8');
+  console.log('Archivo creado correctamente en ./public');
+} else {
+  console.error('La variable de entorno no está definida');
+  process.exit(1);
+}
